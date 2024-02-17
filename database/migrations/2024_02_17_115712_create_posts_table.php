ec1a40b9e7f6aa8eb1011b2fb74c83b2ac47cb78
teshimata <tasuku.tt.bb@gmail.com>
2024-02-17 21:59:56 +0900

    マイグレーション＆シーディング０１

---
+++ database/migrations/2024_02_17_115712_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->string('title', 50);
+            $table->string('body', 500);
+            $table->string('image1', 100)->nullable();
+            $table->string('image2', 100)->nullable();
+            $table->string('image3', 100)->nullable();
+            $table->string('link1', 200)->nullable();
+            $table->string('link2', 200)->nullable();
+            $table->string('link3', 200)->nullable();
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+Run the database seeds.
+     *
+@return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Example User',
+            'age' => 30,
+            'sex' => 1, // 1 が男性、2 が女性を表すと仮定
+            'email' => 'example@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('your_password'), // パスワードは適切にハッシュ化する
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
